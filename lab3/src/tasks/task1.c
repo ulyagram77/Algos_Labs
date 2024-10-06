865133b559ec28484b20575733b3e3463a79ed59
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-10-06 22:37:35 +0000

    lab3 changes

--- lab3/src/tasks/task1.c
+++ lab3/src/tasks/task1.c
@@ -5,7 +5,39 @@
 
 #include "general_utils.h"
 
+void printBinary(unsigned char byte) {
+    for (int i = 7; i >= 0; i--) {
+        int bit = (byte >> i) & 1;
+        printf("%d", bit);
+    }
+    printf(" ");
+}
+
+void printInternalRepresentation(long int longIntValue, long double longDoubleValue, char charValue) {
+    printf("Внутрішнє подання:\n");
+
+    printf("Машинне представлення (long int = %ld): ", longIntValue);
+    for (int i = 0; i < sizeof(long int); i++) {
+        printBinary((unsigned char)(longIntValue >> (i * 8)));
+    }
+    printf("\n");
+
+    printf("Машинне представлення (long double = %.2Lf): ", longDoubleValue);
+    unsigned char *longDoubleBytes = (unsigned char*)&longDoubleValue;
+    for (int i = 0; i < sizeof(long double); i++) {
+        printBinary(longDoubleBytes[i]); 
+    }
+    printf("\n");
+
+    printf("Машинне представлення (char = '%c'): ", charValue);
+    printBinary((unsigned char)charValue);
+    printf("\n");
+}
+
 void task1() {
-//   highlightText("Iterative bisection algorythm:", "blue");
-//   highlightText("Recursive bisection algorythm:", "blue");
+    long int longIntValue = 42L;            
+    long double longDoubleValue = 3.14159L;  
+    char charValue = 'A';                    
+
+    printInternalRepresentation(longIntValue, longDoubleValue, charValue);
 }
