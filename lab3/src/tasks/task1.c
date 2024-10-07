45553b429f8443284e92c27a47a74f519d51c176
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-10-08 00:32:26 +0300

    ended lab 3

--- lab3/src/tasks/task1.c
+++ lab3/src/tasks/task1.c
@@ -13,31 +13,59 @@ void printBinary(unsigned char byte) {
     printf(" ");
 }
 
-void printInternalRepresentation(long int longIntValue, long double longDoubleValue, char charValue) {
-    printf("Внутрішнє подання:\n");
-
-    printf("Машинне представлення (long int = %ld): ", longIntValue);
-    for (int i = 0; i < sizeof(long int); i++) {
-        printBinary((unsigned char)(longIntValue >> (i * 8)));
+void printInternalLongInt(long int val){
+    printf("\033[35mMachine code of (long int = %ld)\033[0m: ", val);
+    for (size_t i = 0; i < sizeof(long int); i++)
+    {
+        printBinary((unsigned char)(val >> (i * 8)));
     }
     printf("\n");
+}
 
-    printf("Машинне представлення (long double = %.2Lf): ", longDoubleValue);
-    unsigned char *longDoubleBytes = (unsigned char*)&longDoubleValue;
+void printInternalLongDouble(long double val){
+    printf("\033[35mMachine code of (long double = %Lf)\033[0m: ", val);
+    unsigned char *longDoubleBytes = (unsigned char*)&val;
     for (int i = 0; i < sizeof(long double); i++) {
         printBinary(longDoubleBytes[i]); 
     }
     printf("\n");
+}
 
-    printf("Машинне представлення (char = '%c'): ", charValue);
-    printBinary((unsigned char)charValue);
+void printInternalChar(char val) {
+    printf("\033[35mMachine code of (char = '%c')\033[0m: ", val);
+    printBinary((unsigned char)val);
     printf("\n");
 }
 
-void task1() {
-    long int longIntValue = 42L;            
-    long double longDoubleValue = 3.14159L;  
-    char charValue = 'A';                    
+void printInternalInt(int val) {
+    printf("\033[35mMachine code of (int = %d)\033[0m: ", val);
+    for (size_t i = 0; i < sizeof(int); i++) {
+        printBinary((unsigned char)(val >> (i * 8)));
+    }
+    printf("\n");
+}
 
-    printInternalRepresentation(longIntValue, longDoubleValue, charValue);
+void printInternalIntArray(int* arr, size_t size) {
+    printf("\033[34mMachine code of array with int values\033[0m:\n\n");
+    for (size_t i = 0; i < size; i++) {
+        printf("Елемент %zu: ", i);
+        printInternalInt(arr[i]);
+    }
+}
+
+void task1() {
+    long int longIntValue = 78L;            
+    long double longDoubleValue = 3.14159L;  
+    char charValue = 'A';  
+
+    int arr[5] = {1, -12, 3, 4, 5};
+
+    highlightText("Machine code of values:", "blue");
+    printf("\n");                  
+
+    printInternalLongInt(longIntValue);
+    printInternalLongDouble(longDoubleValue);
+    printInternalChar(charValue);
+    printf("\n");
+    printInternalIntArray(arr, 5);
 }
