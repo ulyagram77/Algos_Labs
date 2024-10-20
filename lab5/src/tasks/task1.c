951bf3af0d2a2c25fe3ac48627bf0e0c68b8c35f
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-10-20 18:33:26 +0000

    inited lab5

---
+++ lab5/src/tasks/task1.c
@@ -0,0 +1,11 @@
+#include "general_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void task1() {
+
+  highlightText(
+      "Access time testing for structs with alignment and without it: ",
+      "blue");
+}
